c33a922d2bdd2dc01a98260a068becc7f8956e6c
Ralny Andrade <ralnyandrade@gmail.com>
2021-10-21 16:13:21 -0300

    refactor: Filtrar requisição com Middleware

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,32 @@ use App\Http\Livewire\Backend\Apps\Financeiro\FinanceiroCategoriaReceita;
 */
 
 Route::group(['middleware' => ['auth']], function () {
+
+    //Dashboard
     Route::get('/dashboard', 'PagesController@index')->name('dashboard');
+
+    Route::get('/users', UserList::class);
+    //Route::get('/users/cadastro', UserForm::class);
+
+    //Financeiro
+    Route::get('/financeiro/categorias', FinanceiroCategoriaReceita::class);
+
+    //Demo routes metronic
+    // Demo routes
+    Route::get('/datatables', 'PagesController@datatables');
+    Route::get('/ktdatatables', 'PagesController@ktDatatables');
+    Route::get('/select2', 'PagesController@select2');
+    Route::get('/jquerymask', 'PagesController@jQueryMask');
+    Route::get('/icons/custom-icons', 'PagesController@customIcons');
+    Route::get('/icons/flaticon', 'PagesController@flaticon');
+    Route::get('/icons/fontawesome', 'PagesController@fontawesome');
+    Route::get('/icons/lineawesome', 'PagesController@lineawesome');
+    Route::get('/icons/socicons', 'PagesController@socicons');
+    Route::get('/icons/svg', 'PagesController@svg');
+
+    // Quick search dummy route to display html elements in search dropdown (header search)
+    Route::get('/quick-search', 'PagesController@quickSearch')->name('quick-search');
 });
 
-// Demo routes
-Route::get('/datatables', 'PagesController@datatables');
-Route::get('/ktdatatables', 'PagesController@ktDatatables');
-Route::get('/select2', 'PagesController@select2');
-Route::get('/jquerymask', 'PagesController@jQueryMask');
-Route::get('/icons/custom-icons', 'PagesController@customIcons');
-Route::get('/icons/flaticon', 'PagesController@flaticon');
-Route::get('/icons/fontawesome', 'PagesController@fontawesome');
-Route::get('/icons/lineawesome', 'PagesController@lineawesome');
-Route::get('/icons/socicons', 'PagesController@socicons');
-Route::get('/icons/svg', 'PagesController@svg');
 
-// Quick search dummy route to display html elements in search dropdown (header search)
-Route::get('/quick-search', 'PagesController@quickSearch')->name('quick-search');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
