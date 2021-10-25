861c5827581a6e4840e4990deb3e8b76f20bc3b0
Ralny Andrade <ralnyandrade@gmail.com>
2021-10-25 12:07:49 -0300

    refactor: Refatorando Rotas

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
-use App\Http\Livewire\Backend\Apps\User\UserForm;
 use App\Http\Livewire\Backend\Apps\User\UserList;
 use App\Http\Livewire\Backend\Apps\Financeiro\FinanceiroCategoriaReceita;
 
@@ -20,29 +19,16 @@ use App\Http\Livewire\Backend\Apps\Financeiro\FinanceiroCategoriaReceita;
 Route::group(['middleware' => ['auth']], function () {
 
     //Dashboard
-    Route::get('/dashboard', 'PagesController@index')->name('dashboard');
+    Route::get('/dashboard', [PagesController::class, 'index'])->name('dashboard');
 
-    Route::get('/users', UserList::class);
+    //Usuario
+    Route::get('/users', [UserList::class, '__invoke'])->name('users');
 
     //Financeiro
-    Route::get('/financeiro/categorias', FinanceiroCategoriaReceita::class);
-
-    //Demo routes metronic
-    // Demo routes
-    Route::get('/datatables', 'PagesController@datatables');
-    Route::get('/ktdatatables', 'PagesController@ktDatatables');
-    Route::get('/select2', 'PagesController@select2');
-    Route::get('/jquerymask', 'PagesController@jQueryMask');
-    Route::get('/icons/custom-icons', 'PagesController@customIcons');
-    Route::get('/icons/flaticon', 'PagesController@flaticon');
-    Route::get('/icons/fontawesome', 'PagesController@fontawesome');
-    Route::get('/icons/lineawesome', 'PagesController@lineawesome');
-    Route::get('/icons/socicons', 'PagesController@socicons');
-    Route::get('/icons/svg', 'PagesController@svg');
+    Route::get('/financeiro/categorias', [FinanceiroCategoriaReceita::class, '__invoke'])->name('categorias');
 
     // Quick search dummy route to display html elements in search dropdown (header search)
     Route::get('/quick-search', 'PagesController@quickSearch')->name('quick-search');
 });
-
 
 require __DIR__ . '/auth.php';
